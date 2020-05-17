75b4c181cb40e1bc281108bd9570438f52e30964
Geogeo357 <geogeo357@gmail.com>
2020-05-17 04:45:16 -0500

    fixed firebase data
    yay

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD1GYAgBteBkLHKtmmXrvsqKFxoRr2O-R8",
-    authDomain: "work2gather-e8973.firebaseapp.com",
-    databaseURL: "https://work2gather-e8973.firebaseio.com",
-    projectId: "work2gather-e8973",
-    storageBucket: "work2gather-e8973.appspot.com",
-    messagingSenderId: "609218763867",
-    appId: "1:609218763867:web:b13aa86c4dba6b4793337e"
-  };
+  apiKey: "AIzaSyAa--MRkgakfsfNDPsJARvKdVCR-TEKTl0",
+  authDomain: "travis-tutorial.firebaseapp.com",
+  databaseURL: "https://travis-tutorial.firebaseio.com",
+  projectId: "travis-tutorial",
+  storageBucket: "travis-tutorial.appspot.com",
+  messagingSenderId: "587494013644",
+  appId: "1:587494013644:web:bff1f6d5c294c71b59a069"
+};
   
   firebase.initializeApp(firebaseConfig);
 
